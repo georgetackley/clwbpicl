8ceac61dc8fa490f62d66fea4f1b236d5d8c1333
George Tackley <tackley@gmail.com>
2026-04-13 11:43:09 +0100

    ...

--- app.R
+++ app.R
@@ -700,7 +700,7 @@ ui <- page_fluid(
   div(img(src='logo.png', width="20%",style="max-width:150px; height:auto; padding:15px;"), style="text-align: center;"),
   titlePanel(h1("Clwb Picl Stats", align="center")),
   div(tags$a("Return to Main site",href="https://cardiffpickleballclub.wordpress.com/"), style="text-align: center;"),
-  actionButton("update","Update"),
+  actionButton("update","Update",align="right"),
   
   # Leaderboards:
   card(card_header("Ladder Leaderboards"),
