7ea88091342c62ca9c9d09718fe7731c5f857b11
George Tackley <tackley@gmail.com>
2026-05-09 22:45:02 +0100

    ...

--- app.R
+++ app.R
@@ -733,16 +733,22 @@ ui <- page_fluid(
     #                        align="center"),column(4)),
   
   # Leaderboards:
-  card(card_header("Ladder Leaderboards"),
-       div(p(paste0("(Last updated: ",as_date(ymd_hms(max(match_table$date_time))),")"),style="font-size: 12px;")),
   layout_columns(
+    card(),
+  card(card_header("CPC Common Ladder"),
+       div(p(paste0("(Last updated: ",as_date(ymd_hms(max(match_table$date_time))),")"),style="font-size: 12px;")),
+  #layout_columns(
     #card(card_header("LLC Thursday"),
     #     formattableOutput("thu_ladder")),
-    card(),
-    card(card_header("CPC Common Ladder"),
-               formattableOutput("cpc_ladder")),
-    card(),
-  )),
+    #card(),
+    #card(
+      #card_header("CPC Common Ladder"),
+               formattableOutput("cpc_ladder")#),
+    #card(),
+    #)
+  ),
+  card()
+  ),
   # Selection drop-downs:
   card(
   layout_column_wrap(
