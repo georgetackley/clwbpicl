49961aed5c81b5c85a5cad4330dcd9e3500d1594
George Tackley <tackley@gmail.com>
2026-09-07 15:21:58 +0100

    ...

--- app.R
+++ app.R
@@ -890,8 +890,9 @@ ui <- page_fluid(
   ),
   #div(img(src='logo-text.svg', width="100%"))),
   layout_columns(
-    card(class = "bg-primary", card_header("Female 4DRs"),
-         formattableOutput("cpc_ladder_F")
+    card(card_header("Female 4DRs"),
+         formattableOutput("cpc_ladder_F"),
+         class = "bg-primary"
     ),
     card(card_header("Male 4DRs"),
          formattableOutput("cpc_ladder_M")
